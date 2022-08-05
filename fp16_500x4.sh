08ac9777ccfdcad59b475fb3ffcaf83003c2b93e
Bruce Ray Wilson <BruceRayWilson@anl.gov>
2022-08-05 14:58:24 -0600

    More fp16 bash scripts.

--- fp16_500x4.sh
+++ fp16_500x4.sh
@@ -1,6 +1,6 @@
 for i in {1..5}
 do
-    name=float16_3x500_${i}
+    name=float16_4x500_${i}
     python bin/exec.py \
     mode=train \
     run.id=${name} \
@@ -8,7 +8,7 @@ do
     data.data_directory=/lambda_stor/data/datascience/cosmic_tagging/ \
     framework=torch \
     run.compute_mode=HPU \
-    run.minibatch_size=3 \
+    run.minibatch_size=4 \
     run.iterations=500 \
     run.precision=3 > ${name}.log 2>&1 &
 done
