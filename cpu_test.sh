7e7ba594a1caee0d5a5457c7639df0fb62893430
Bruce Ray Wilson <BruceRayWilson@anl.gov>
2023-02-03 18:36:35 +0000

    NFS mount results

--- cpu_test.sh
+++ cpu_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # git checkout Graphcore
-for i in {1}
+for i in {1,}
 do
     name=bfloat16_2x10_${i}
     python bin/exec.py \
@@ -15,15 +15,3 @@ do
     run.precision=3 \
     > ${name}.log 2>&1 &
 done
-
-
-python3.8 bin/exec.py \
-mode=train \
-run.id=01 \
-run.distributed=False \
-data.data_directory=/lambda_stor/data/datascience/cosmic_tagging/ \
-framework=torch \
-run.compute_mode=CPU \
-run.minibatch_size=1 \
-run.iterations=1 \
-run.precision=3
--- ipu_test_popdist.sh
+++ ipu_test_popdist.sh
@@ -2,21 +2,22 @@
 # git checkout Graphcore
 for i in {1,}
 do
+    name=bfloat16_2x10_${i} \
     poprun \
     -vv \
-    --num-instances=4 \
-    --num-replicas=4 \
-    --executable-cache-path=$PYTORCH_CACHE_DIR
-    name=bfloat16_2x10_${i}
+    --num-instances=1 \
+    --num-replicas=1 \
+    --executable-cache-path=$PYTORCH_CACHE_DIR \
     python bin/exec.py \
+    --config-name=a21 \
     mode=train \
     run.id=${name} \
     run.distributed=False \
+    data=real \
     data.data_directory=/lambda_stor/data/datascience/cosmic_tagging/ \
     framework=torch \
     run.compute_mode=IPU \
     run.minibatch_size=2 \
     run.iterations=10 \
-    run.precision=3 \
-    > ${name}.log 2>&1 &
+    run.precision=3
 done
