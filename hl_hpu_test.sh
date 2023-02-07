e40f99155c9b3074bc7e295bf34d8220cec18f19
Bruce Ray Wilson <BruceRayWilson@anl.gov>
2023-02-07 16:13:34 +0000

    hl_hpu_test.sh.

---
+++ hl_hpu_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# git checkout Graphcore
+python3.8 bin/exec.py \
+--config-name=a21 \
+mode=train \
+run.id=01 \
+run.distributed=False \
+data=real \
+data.data_directory=/lambda_stor/data/datascience/cosmic_tagging/ \
+framework=torch \
+run.compute_mode=HPU \
+run.precision=3 \
+hydra/job_logging=disabled
