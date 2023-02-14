d90e858e2f898904a1459b0d127828e4f4c009b9
Bruce Ray Wilson <BruceRayWilson@anl.gov>
2023-02-14 20:43:55 +0000

    Updated master.sh.

--- master.sh
+++ master.sh
@@ -1,3 +1,6 @@
 rm -rf outputs
 rm bfloat16_*.log
 source hpu_test_001.sh > bfloat16_A21_1.log 2>&1
+source hpu_test_002.sh > bfloat16_A21_2.log 2>&1
+source hpu_test_004.sh > bfloat16_A21_4.log 2>&1
+source hpu_test_008.sh > bfloat16_A21_8.log 2>&1
