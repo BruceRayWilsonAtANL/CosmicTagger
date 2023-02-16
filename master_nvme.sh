c3656328f6c63a3e637248ee30a969bdc16c1f08
Bruce Ray Wilson <BruceRayWilson@anl.gov>
2023-02-16 16:28:07 +0000

    Updated master_nvme.sh.

--- master_nvme.sh
+++ master_nvme.sh
@@ -1,8 +1,8 @@
 rm -rf outputs
 rm bfloat16_*.log
+source ipu_test_popdist_64.sh > bfloat16_2x10_64_nvme.log 2>&1
 source ipu_test_popdist_1.sh > bfloat16_2x10_1_nvme.log 2>&1
 source ipu_test_popdist_2.sh > bfloat16_2x10_2_nvme.log 2>&1
 source ipu_test_popdist_4.sh > bfloat16_2x10_4_nvme.log 2>&1
 source ipu_test_popdist_8.sh > bfloat16_2x10_8_nvme.log 2>&1
 source ipu_test_popdist_16.sh > bfloat16_2x10_16_nvme.log 2>&1
-source ipu_test_popdist_64.sh > bfloat16_2x10_64_nvme.log 2>&1
